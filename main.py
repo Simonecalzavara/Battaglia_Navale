075e601b61690d60084aeba7af0a261109aac0ac
Your Name <simonecalza98@gmail.com>
2022-02-25 18:16:48 +0100

    L inserimento delle navi ora avviene in maniera indipendente dall utente

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import board as b
 from Ship_class import *
 from variable import *
 from time import sleep
-#scelta dell'utente riguardo alle dimensioni della scacchiera
+
+print("Benvenuto ")
 
 #variabili
-n_ins=input('inserisci le dimensioni del tavolo da gioco:  ')
+n_ins=input('Inserisci le dimensioni del tavolo da gioco:  ')
 row_size=int(n_ins)
 col_size=row_size
 
@@ -20,52 +21,21 @@ clearconsole=lambda: print('\n' * 150)
 clearconsole()
 
 #fase di inserimento delle navi
-counter_navi=0
-while counter_navi<numero_navi:
-    print("""\nHai a disposizione 5 navi, puoi inserire ciascuna nave soltanto una volta.
-    Portaerei, la puoi inserire in 5 caselle
-    Corazzata, la puoi inserire in 4 caselle
-    Incrociatore, la puoi inserire in 3 caselle
-    sottomarino, la puoi inserire in 3 caselle
-    Cacciatorpediniere la puoi inserire in 2 caselle""")
-    nave_=input("\n\ninserisci la nave che vuoi piazzare:  ")
-    match nave_:
-        case "portaerei":
-            if counter_p==0:
-                b.board_add(portaerei,legenda_asse_orizzontale_iniziale,board,board_display)
-                counter_p += 1
-            else:
-                print("hai gia inserito questa nave. Inserisci una nuova nave")
-        case "corazzata":
-            if counter_c==0:
-                b.board_add(corazzata,legenda_asse_orizzontale_iniziale,board,board_display)
-                counter_c+=1
-            else:
-                print("hai gia inserito questa nave. Inserisci una nuova nave")
-        case "incrociatore":
-            if counter_i==0:
-                b.board_add(incrociatore,legenda_asse_orizzontale_iniziale,board,board_display)
-                counter_i+=1
-            else:
-                print("hai gia inserito questa nave. Inserisci una nuova nave")
-        case "sottomarino":
-            if counter_s==0:
-                b.board_add(sottomarino,legenda_asse_orizzontale_iniziale,board,board_display)
-                counter_s=+1
-            else:
-                print("hai gia inserito questa nave. Inserisci una nuova nave")
-        case "cacciatorpediniere":
-            if counter_ca==0:
-                b.board_add(cacciatorpediniere,legenda_asse_orizzontale_iniziale,board,board_display)
-                counter_ca+=1
-            else:
-                print("hai gia inserito questa nave. Inserisci una nuova nave")
-        case _:
-            print("inserisci una nave valida.")
+print("""\nHai a disposizione 5 navi, puoi inserire ciascuna nave soltanto una volta.
+Portaerei, la puoi inserire in 5 caselle
+Corazzata, la puoi inserire in 4 caselle
+Incrociatore, la puoi inserire in 3 caselle
+sottomarino, la puoi inserire in 3 caselle
+Cacciatorpediniere la puoi inserire in 2 caselle""")
+
+print("\nLe navi verranno inserite in maniera crescente e secondo la loro dimensione.")
+ship_list=[cacciatorpediniere,sottomarino,incrociatore,corazzata,portaerei]
+for ship in ship_list:
+    b.print_board(board,row_size)
+    b.board_add(ship,legenda_asse_orizzontale_iniziale,board,row_size,col_size)
     clearconsole()
-    b.print_board(board_display, row_size)
-    counter_general=[counter_p,counter_c,counter_i,counter_ca,counter_s]
-    counter_navi=sum(counter_general)
+
+b.print_board(board,row_size)
 
 #visualizzazione delle navi piazzate
 b.print_board(board_display,row_size)
