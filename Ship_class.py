d763800769c8a223b4e112743418b7660c9e299d
Your Name <simonecalza98@gmail.com>
2022-09-09 17:38:05 +0200

    Correzione del metodo inserimento

--- Ship_class.py
+++ Ship_class.py
@@ -1,38 +1,37 @@
 class Navi:
     # creazione delle navi da gioco
 
-    def __init__(self, dimensione):
+    def __init__(self, dimensione,name):
         self.dimensione = dimensione
+        self.name=name
         self.coordinate = []
 
     #metodo della classe che permette l'inserimento delle coordinate della nave
-    def inserimento(self, coord_col, coord_rig,row_size,col_size,board):
-        orientamento_=input("Inserisci la maniera con cui vuoi inserire la nave. Orizzontale o Verticale?\n\nInserisci una delle due scelte: ")
-        orientamento=orientamento_.casefold()
+    def inserimento(self, coord_col, coord_rig,row_size,col_size,board,orientamento):
         index=0
         match orientamento:
             case "orizzontale":
-                if coord_rig in range(row_size):
+                if coord_col+self.dimensione-1 in range(1,row_size+1):
                     while index in range(self.dimensione) :
-                        if coord_col + index in range(col_size):   #Inserimento delle coordinate in maniera automatica. Inserisce verso dx
+                        if coord_col + index in range(1,col_size+1):   #Inserimento delle coordinate in maniera automatica. Inserisce verso dx
                             self.coordinate.append([coord_col+index,coord_rig])
                             index=index+1
                 else:
-                    print("\nLa coordinata e' fuori dalla scacchiera. Inserisci una coordinata valida")
+                    print("\n\u001b[31mLa coordinata è fuori dalla scacchiera. Inserisci una coordinata valida\033[0m")
             case "verticale":
-                if coord_col in range(col_size):
+                if coord_rig+self.dimensione-1 in range(1,col_size+1):
                     while index in range(self.dimensione):
-                        if coord_rig + index in range(row_size):   #Inserimento delle coordinate in maniera automatica. Inserisce verso il basso
+                        if coord_rig + index in range(1,row_size+1):   #Inserimento delle coordinate in maniera automatica. Inserisce verso il basso
                             self.coordinate.append([coord_col,coord_rig+index])
                             index=index+1
                 else:
-                    print("\nLa coordinata e' fuori dalla scacchiera. Inserisci una coordinata valida")
+                    print("\n\u001b[31mLa coordinata è fuori dalla scacchiera. Inserisci una coordinata valida\033[0m")
             case _:
                 print("\nInserisci il valore corretto di inserimento. Orizzontalmente o Verticalmente")
         if not self.esiste_posizione(board):
             if len(self.coordinate)==self.dimensione:
                 self.riempimento(board)
-                return 1
+                return True
         else:
             self.coordinate=[]
             print("\nLa nave che stai inserendo colliderebbe con un alta nave giá piazzata. Inserisci la prima coordinata della nave in una casella valida.")
