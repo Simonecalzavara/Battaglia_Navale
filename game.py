5ffd9e498fd63d89b5a8b3749b5faa0aa2e1c14d
Your Name <simonecalza98@gmail.com>
2022-09-13 22:44:20 +0200

    funzione che permette la fase di attacco delle navi

--- game.py
+++ game.py
@@ -1,5 +1,23 @@
 import board
 import os
+import time
+
+def hit_guess(board_display,row_size,col_size,ship_list):
+    guess=((input("\nInserisci le coordinate per l'attacco: ")).replace(""," ")).upper()
+    guess_col,guess_row=board.coord_type_change(guess)
+    hit_row=attack_row(guess_row,row_size)
+    hit_col=attack_col(guess_col,col_size)
+    for ship in ship_list:
+        if ship.hit(hit_col,hit_row):
+            print("Hai colpito la nave!")
+            board_display[guess_col-1][guess_row-1]='*'
+            time.sleep(2)
+            return True
+    print("Non hai colpito nessuna nave")
+    board_display[guess_col - 1][guess_row - 1] = 'X'
+    time.sleep(2)
+    return False
+
 
 def attack_row(guess_row,row_size_):
     try:
