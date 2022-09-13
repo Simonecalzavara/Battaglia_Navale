98d6aea83dfa9cd048006f2b169454e6bba6772d
Your Name <simonecalza98@gmail.com>
2022-09-13 22:50:47 +0200

    modifica delle variabili di gioco

--- variable.py
+++ variable.py
@@ -1,15 +1,10 @@
 from Ship_class import *
 
 legenda_asse_orizzontale_iniziale={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H',9:'I',10:'L',11:'M',12:'N',13:'O',14:'P',15:'Q'}
-portaerei=Navi(5)
-corazzata=Navi(4)
-incrociatore=Navi(3)
-sottomarino=Navi(3)
-cacciatorpediniere=Navi(2)
-i=0
+portaerei=Navi(5,"portaerei")
+corazzata=Navi(4,"corazzata")
+incrociatore=Navi(3,"incrociatore")
+sottomarino=Navi(3,"sottomarino")
+cacciatorpediniere=Navi(2,"cacciatorpediniere")
 numero_navi=5
-counter_p=0
-counter_i=0
-counter_s=0
-counter_ca=0
-counter_c=0
+ship_list=[cacciatorpediniere,sottomarino,incrociatore,corazzata,portaerei]         #creazione della lista delle navi disponibili al giocatore
