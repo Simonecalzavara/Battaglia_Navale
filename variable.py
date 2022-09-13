cee5ecb4e406c4445c528a7a5bb9f36476b29ee9
Your Name <simonecalza98@gmail.com>
2022-09-13 22:53:29 +0200

    aggiunta del argparse per la selezione del giocatore relativa alle dimensioni del tavolo da gioco

--- variable.py
+++ variable.py
@@ -1,4 +1,6 @@
 from Ship_class import *
+import argparse
+import sys
 
 legenda_asse_orizzontale_iniziale={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H',9:'I',10:'L',11:'M',12:'N',13:'O',14:'P',15:'Q'}
 portaerei=Navi(5,"portaerei")
@@ -8,3 +10,37 @@ sottomarino=Navi(3,"sottomarino")
 cacciatorpediniere=Navi(2,"cacciatorpediniere")
 numero_navi=5
 ship_list=[cacciatorpediniere,sottomarino,incrociatore,corazzata,portaerei]         #creazione della lista delle navi disponibili al giocatore
+giocatore1='Giocatore1'
+giocatore2='Giocatore2'
+
+def initialize_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-r", "--rows",
+                        help="Number of rows of the board",
+                        type=int,
+                        default=9)
+
+    parser.add_argument("-c", "--columns",
+                        help="Number of columns of the board",
+                        type=int,
+                        default=9)
+
+
+    return parser.parse_args()
+
+def check_parser(args):
+    try:
+        check_arguments(args)
+    except ValueError:
+        sys.exit()
+
+
+def check_arguments(args):
+    if not 0 < args.rows < 15:
+        print('\u001b[31mInvalid number of rows\033[0m')
+        raise ValueError
+    if not 0 < args.columns < 15:
+        print('\u001b[31mInvalid number of columns\033[0m')
+        raise ValueError
+
