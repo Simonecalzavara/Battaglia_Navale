837d646e61cb5f2588ab33c51803ef927fcab542
Your Name <simonecalza98@gmail.com>
2022-09-13 23:47:41 +0200

    deepcopy della lista delle navi a disposizione per il giocatore 2

--- variable.py
+++ variable.py
@@ -1,6 +1,7 @@
 from Ship_class import *
 import argparse
 import sys
+import copy
 
 legenda_asse_orizzontale_iniziale={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H',9:'I',10:'L',11:'M',12:'N',13:'O',14:'P',15:'Q'}
 portaerei=Navi(5,"portaerei")
@@ -10,6 +11,7 @@ sottomarino=Navi(3,"sottomarino")
 cacciatorpediniere=Navi(2,"cacciatorpediniere")
 numero_navi=5
 ship_list=[cacciatorpediniere,sottomarino,incrociatore,corazzata,portaerei]         #creazione della lista delle navi disponibili al giocatore
+ship_list_2=copy.deepcopy(ship_list)                                                #creazione di una seconda lista di navi
 giocatore1='Giocatore1'
 giocatore2='Giocatore2'
 
@@ -43,4 +45,3 @@ def check_arguments(args):
     if not 0 < args.columns < 15:
         print('\u001b[31mInvalid number of columns\033[0m')
         raise ValueError
-
