45381655c88166d0a63f1773335c8c87e8ee577c
Your Name <simonecalza98@gmail.com>
2024-01-17 13:10:34 +0100

    update minore

--- board.py
+++ board.py
@@ -6,6 +6,12 @@ import re
 
 #funzione per visualizzare a schermo il campo da gioco
 def print_board(board_array, row_size):
+    """
+    Funzione che permette la visualizzazione a schermo del campo da gioco
+    :param board_array: array contenente le informazioni relative allo stato del tavolo da gioco del giocatore
+    :param row_size: massima dimensione delle righe del tavolo da gioco
+    :return: print del tavolo da gioco
+    """
     # Creazione delle coordinate in maniera dinamica
     legenda_asse_orizzontale = ''
     for i in legenda_asse_orizzontale_iniziale:
@@ -20,6 +26,14 @@ def print_board(board_array, row_size):
 
 #funzione che permette l'inserimento di una nave all'interno del tavolo
 def board_add(player,ship_list,row_size,col_size):
+    """
+    Funzione che consente l'inserimento delle navi all'interno del tavolo da gioco
+    :param player: variabile associata al giocatore che deve inserire la propria lista di navi all'interno del tavolo da gioco
+    :param ship_list: lista delle navi da inserire all'interno del tavolo da gioco
+    :param row_size: massima dimensione delle righe del tavolo da gioco
+    :param col_size: massima dimensione delle colonne del tavolo da gioco
+    :return:
+    """
     board = [[0] * col_size for x in range(row_size)]
     print(player, 'é il tuo turno di inserire le navi')
     time.sleep(3)
@@ -47,6 +61,11 @@ def board_add(player,ship_list,row_size,col_size):
     return board
 
 def coord_type_change(coordinate):
+    """
+    Funzione che consente di cambiare le coordinate inserite in formato numerico
+    :param coordinate: stringa contente le coordinate in formato alfanumerico
+    :return: col: contiene la coordinata associata alle colonne all'interno del tavolo da gioco rig: contiene la coordinata associata alle righe all'interno del tavolo da gioco
+    """
     for key in legenda_asse_orizzontale_iniziale:
         try:
             letter = coordinate.find(legenda_asse_orizzontale_iniziale.get(key))
