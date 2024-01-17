f2685a553d1ea9b3c24b90e5aae753ff8e53e5d5
Your Name <simonecalza98@gmail.com>
2024-01-17 12:34:03 +0100

    update minore

--- Ship_class.py
+++ Ship_class.py
@@ -11,6 +11,16 @@ class Navi:
 
     #metodo della classe che permette l'inserimento delle coordinate della nave
     def inserimento(self, coord_col, coord_rig,row_size,col_size,board,orientamento):
+        """
+        Metodo della classe che permette l'inserimento delle coordinate della nave
+        :param coord_col: coordinata delle colonne associate all'inserimento della nave
+        :param coord_rig: coordinata delle righe associate all'inserimento della nave
+        :param row_size: dimensione del tavolo da gioco associata alle righe
+        :param col_size: dimensione del tavolo da gioco associata alle colonne
+        :param board: tavolo da gioco associato al giocatore
+        :param orientamento: modalitá di inserimento della nave. La nave puó essere inserita in maniera vertical o in maniera orizzontale
+        :return: Restituisce il valore True se la nave é stata inserita in maniera corretta all'interno del tavolo da gioco, altrimenti False
+        """
         index=0
         match orientamento:
             case "orizzontale":
@@ -44,6 +54,10 @@ class Navi:
 
     #metodo che permette di visualizzare lo stato della nave
     def stato(self):
+        """
+        metodo della classe Ship che restituisce lo stato della nave
+        :return: print dello stato attuale della nave
+        """
         if len(self.coordinate) == 0:
             print('La nave é stata affondata')
         elif len(self.coordinate) == self.dimensione:
@@ -52,27 +66,53 @@ class Navi:
             print('La nave é stata colpita')
 
     def riempimento(self,board):      #riempimento del tavolo con le coordinate inserite della nave
+        """
+        Metodo della classe nave che riempie la scacchiera in base alle coordinate inserite dal giocatore
+        :param board: tavolo da gioco del giocatore
+        """
         for coordinate_rig,coordinate_col in self.coordinate:
             board[coordinate_col - 1 ][coordinate_rig - 1]=1
 
     def esiste_posizione(self,board):      #funzione che restituisce se tutte le coordinate della nave sono presenti all'interno del tavolo
+        """
+        Metodo della nave che restituisce se tute le coordinate della nave sono presenti all'interno del tavolo da gioco
+        :param board: tavolo da gioco del giocatore
+        :return: True se tutte le coordinate della nave sono presenti all'interno del tavolo da gioco, altrimenti False
+        """
         for coordinate_rig,coordinate_col in self.coordinate:
             if board[coordinate_col-1][coordinate_rig-1]==1:
                 return True
         return False
 
     def hit(self,guess_row,guess_col):      #metodo che elimina le coordinate dalla nave se viene colpita
+        """
+        :param guess_row: coordinata delle righe associata al colpo
+        :param guess_col: coordinata delle colonne associata al colpo
+        :return: True se una coordinata della nave é stata colpita altrimenti False
+        """
         if [guess_col,guess_row] in self.coordinate:
             self.coordinate.remove([guess_col,guess_row])
             return True
         return False
 
     def esiste_vicino(self,board,col_size,row_size):                                  #controllo della presenza di navi nel intorno della nave che sta per essere piazzata
+        """
+        Metodo della classe per effettuare un check sulla presenza di navi vicine durante l'inserimento di una nave sul tavolo da gioco
+        :param board: tavolo da gioco del giocatore
+        :param col_size: massima dimensione delle colonne del tavolo da gioco
+        :return: True se esiste una coordinata di un altra nave vicino alla nave che sta per essere piazzata, altrimenti False
+        """
         if self.esiste_vicino_verticale(board,col_size) or self.esiste_vicino_orizzontale(board,row_size):
             return True
         return False
 
     def esiste_vicino_verticale(self,board,col_size):
+        """
+        Metodo della classe per effettuare un check verticalmente sulla presenza di navi vicine durante l'inserimento di una nave sul tavolo da gioco
+        :param board: tavolo da gioco del giocatore
+        :param col_size: massima dimensione delle colonne del tavolo da gioco
+        :return: True se esiste una coordinata di un altra nave vicino alla nave che sta per essere piazzata, altrimenti False
+        """
         for coordinate_col,coordinate_rig in self.coordinate:
             if coordinate_col== 1 and board[coordinate_rig-1][coordinate_col]==1:     #nave inserita nella prima colonna e controllo esistenza nave a destra
                 return True
@@ -87,7 +127,7 @@ class Navi:
 
     def esiste_vicino_orizzontale(self,board,row_size):
         """
-
+        Metodo della classe per effettuare un check orizzontalmente sulla presenza di navi vicine durante l'inserimento di una nave sul tavolo da gioco
         :param board: tavolo da gioco del giocatore
         :param row_size: massima dimensione delle righe del tavolo da gioco
         :return: True se esiste una coordinata di un altra nave vicino alla nave che sta per essere piazzata, altrimenti False
